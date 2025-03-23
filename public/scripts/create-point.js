fa9efff543a64d019796b8dbed163fe7ddabb55f
Valdemir Filho <filhux@gmail.com>
2025-03-23 19:48:23 -0300

    update packages

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -2,9 +2,9 @@ function populateUFs() {
   const ufSelect = document.querySelector("select[name=uf]")
 
   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
-    .then( res =>  res.json() )
-    .then( states => {
-      for( state of states ) {
+    .then(res => res.json())
+    .then(states => {
+      for (state of states) {
         ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
       }
     })
@@ -22,27 +22,29 @@ function getCities(event) {
   const indexOfSelectedState = event.target.selectedIndex
   stateInput.value = event.target.options[indexOfSelectedState].text
 
+  console.log(ufValue)
+
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
-  citySelect.innerHTML = `<option value="">Selecione a Cidade</option>` 
+  citySelect.innerHTML = `<option value="">Selecione a Cidade</option>`
   citySelect.disabled = true
 
   fetch(url)
-  .then( res => res.json() )
-  .then( cities => {
+    .then(res => res.json())
+    .then(cities => {
 
-    // stateInput.value = cities[event.target.selectedIndex].microrregiao.mesorregiao.UF.nome
+      // stateInput.value = cities[event.target.selectedIndex].microrregiao.mesorregiao.UF.nome
 
-    for ( const city of cities ) {
-      // citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-      const newElement = document.createElement('option')
-      newElement.value = city.nome
-      newElement.innerText = city.nome
-      optCities.appendChild(newElement)
-    }
-    citySelect.disabled = false;
-    citySelect.appendChild(optCities)
-  })
+      for (const city of cities) {
+        // citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        const newElement = document.createElement('option')
+        newElement.value = city.nome
+        newElement.innerText = city.nome
+        optCities.appendChild(newElement)
+      }
+      citySelect.disabled = false;
+      citySelect.appendChild(optCities)
+    })
 }
 
 document
@@ -69,16 +71,16 @@ function handleSelectedItem(event) {
 
   // verificar se existem itens selecionados, se sim
   // pegar os itens selecionados
-  const alreadySelected = selectedItems.findIndex( item => { 
+  const alreadySelected = selectedItems.findIndex(item => {
     const itemFound = item == itemId
     return itemFound
   })
 
   // se já estiver selecionado, tirar da seleção
-  if ( alreadySelected >= 0 ) {
-    const filteredItems = selectedItems.filter( item => {
+  if (alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter(item => {
       const itemIsDifferent = item != itemId
-      return itemIsDifferent 
+      return itemIsDifferent
     })
 
     selectedItems = filteredItems
@@ -89,7 +91,5 @@ function handleSelectedItem(event) {
   // atualizar o campo escondido com os itens selecionados
   collectedItems.value = selectedItems.toString().replace(/,/g, ", ")
 
-  
+
 }
-
-
